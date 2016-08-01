5f7095ee9aee44d96df4791ad0949347f15e809c
rockbot <raquel@rckbt.me>
2016-08-01 10:45:14 -0700

    first commit woo

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const P = require('bluebird');
+
+const redis = require('./lib/redis');
+
+module.exports = P.promisifyAll(require('generic-pool').Pool({
+  name: 'redis',
+  max: 10,
+  min: 1,
+  idleTimeoutMillis: 30000,
+  create: function(cb) {
+    var client = redis.createClient(process.env.REDIS_URL);
+    client.on('connect', function() {
+      client.removeListener('error', cb);
+      cb(null, client);
+    });
+
+    client.on('error', cb);
+  },
+  destroy: function(client) {
+    client.end();
+  }
+}));
+
+module.exports.withConnection = function(liftedFn) {
+  var redisP = this.acquireAsync();
+  return redisP.then(conn => {
+    return redisP.then(liftedFn).finally(x => this.release(conn))
+  });
+};
